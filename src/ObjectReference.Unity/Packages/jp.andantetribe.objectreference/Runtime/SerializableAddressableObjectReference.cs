17c0d0ac94501b4775cadf81dbd94dd3959d1eeb
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-03-01 18:27:15 +0900

    feat: initial up

---
+++ src/ObjectReference.Unity/Packages/jp.andantetribe.objectreference/Runtime/SerializableAddressableObjectReference.cs
@@ -0,0 +1,48 @@
+﻿#if ENABLE_ADDRESSABLES && ENABLE_UNITASK
+#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace ObjectReference
+{
+    [Serializable]
+    internal sealed class SerializableAddressableObjectReference<T> : IObjectReference<T> where T : UnityEngine.Object
+    {
+        [SerializeField]
+        private AssetReferenceT<T> _value = null!;
+        private T? _cached;
+
+        /// <inheritdoc />
+        public async ValueTask<T> LoadAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            _cached ??= await _value.LoadAssetAsync<T>().ToUniTask(cancellationToken: cancellationToken, autoReleaseWhenCanceled: true);
+            return _cached;
+        }
+
+        /// <inheritdoc />
+        public async ValueTask<T> LoadAsync(IProgress<float> progress, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            _cached ??= await _value.LoadAssetAsync<T>().ToUniTask(progress: progress, cancellationToken: cancellationToken, autoReleaseWhenCanceled: true);
+            return _cached;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            if (_cached != null)
+            {
+                _value.ReleaseAsset();
+                _cached = null;
+            }
+        }
+    }
+}
+
+#endif
